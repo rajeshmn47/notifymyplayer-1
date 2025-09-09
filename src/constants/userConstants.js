90bbf99eb36b6f56b7fcb3ebd86540d724b3bcc4
rajeshmn47 <rajeshmn47@gmail.com>
2025-09-09 08:04:16 +0530

    fix backend url

--- src/constants/userConstants.js
+++ src/constants/userConstants.js
@@ -61,7 +61,7 @@ function geturl() {
         // return "https://backendforpuand-dream11.onrender.com";
         // return 'https://apk.thepowerplay.co';
         // return "http://31.97.61.35:3000";
-         return "https://fango11.duckdns.org"
+        // return "https://fango11.duckdns.org"
         // return "http://localhost:8000";
         // return 'http://192.168.237.175:8000';
         // return 'http://46.28.44.4:8000';
@@ -69,7 +69,7 @@ function geturl() {
         // return "https://fantasycricket4u.com";
         // return "https://backend.dreambet11.io";
         // return "https://gamizo.duckdns.org";
-        // return "https://mangogames.fun/api";
+        return "https://mangogames.fun/api";
     }
     // return "https://dreamelevenclonebackend-7l34.onrender.com";
     // return "https://dreamelevenclonebackend-1v8e.onrender.com";
@@ -84,7 +84,8 @@ function geturl() {
     // return "https://fantasycricket4u.com";
     // return "https://dreamelevenclonebackend-g4iw.onrender.com"
     // return "https://backend.dreambet11.io";
-    return "https://gamizo.duckdns.org";
+    // return "https://gamizo.duckdns.org";
+    return "https://mangogames.fun/api";
 }
 
 function getfrontendurl() {
