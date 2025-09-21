a5d2c8ea28452d4c8266ee7c4cce1d23cbfc5819
rajeshmn47 <rajeshmn47@gmail.com>
2025-09-21 10:10:40 +0530

    addednew backend url

--- src/constants/userConstants.js
+++ src/constants/userConstants.js
@@ -53,7 +53,7 @@ export const DELETE_USER_RESET = 'DELETE_USER_RESET';
 export const CLEAR_ERRORS = 'CLEAR_ERRORS';
 
 function geturl() {
-    const current = "local";
+    const current = "locale";
 
     if (current == 'local') {
         // return "http://54.172.255.164";
@@ -68,8 +68,8 @@ function geturl() {
         // return "https://dreamelevenclonebackend-g4iw.onrender.com";
         // return "https://fantasycricket4u.com";
         // return "https://backend.dreambet11.io";
-        // return "https://gamizo.duckdns.org";
-        return "https://mangogames.fun/api";
+        return "https://fango11.duckdns.org";
+        // return "https://mangogames.fun/api";
     }
     // return "https://dreamelevenclonebackend-7l34.onrender.com";
     // return "https://dreamelevenclonebackend-1v8e.onrender.com";
@@ -79,13 +79,13 @@ function geturl() {
     // return "http://dream-env.eba-6d6ds7up.us-east-1.elasticbeanstalk.com";
     // return "http://54.172.255.164";
     // return 'https://apk.thepowerplay.co';
-    //return 'http://localhost:8000';
+    return 'http://localhost:8000';
     // return 'http://46.28.44.4:8000';
     // return "https://fantasycricket4u.com";
     // return "https://dreamelevenclonebackend-g4iw.onrender.com"
     // return "https://backend.dreambet11.io";
     // return "https://gamizo.duckdns.org";
-    return "https://mangogames.fun/api";
+    // return "https://mangogames.fun/api";
 }
 
 function getfrontendurl() {
